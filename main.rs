83c40f5b587a04ebb66feda86086ed9fbb04a801
Grimhiker <66091618+Grimhiker@users.noreply.github.com>
2020-06-10 09:57:26 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut x = 4520;
+
+    println!("the value of x is {}", x);
+
+    x = 10; 
+
+    println!("the value of x is {}", x);
+
+    loop {x =33;
+
+    if x > 40 {println!("I like {}", x);
+    } 
+    else {println!("I see");
+    break}
+
+}
+x = 42;
+
+println!("the answer to everything is {}", x);
+}
